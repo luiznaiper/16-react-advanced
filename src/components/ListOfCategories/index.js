a6f9e7045369162a29f95e0faf6b923c80f82da0
luiznaiper <luiz_naiper@hotmail.com>
2022-07-05 07:36:01 -0500

    :construction: Install axios, use hooks, change lint in api
    Using hooks useEffect and useStatee to fetch categories in the project, changing uselint json to deploy the backend api, installing axios and using asiync await

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Category } from '../Categoy'
+import React, {useState, useEffect} from 'react'
+import { Category } from '../Category'
 import {List, Item} from './styles'
-import data from '../../../api/db.json'
+import axios from 'axios'
+
+
 
 const ListOfCategories = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        const fetchCategories = async () => {
+          const { data } = await axios.get(
+            "https://16-react-advanced-luiznaiper.vercel.app/categories"
+          );
+          setCategories(data);
+        };
+    
+        fetchCategories();
+      }, []);
     return (
         <List>
             {
-                data.categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
+                categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
             }
         </List>
     )
