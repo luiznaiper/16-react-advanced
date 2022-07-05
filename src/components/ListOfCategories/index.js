9dbf0c52a572cdad4ac5c6044a42a9b0b42c6777
luiznaiper <luiz_naiper@hotmail.com>
2022-07-05 07:56:48 -0500

    :construction: Added fixed categories with useEffect

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -7,6 +7,7 @@ import axios from 'axios'
 
 const ListOfCategories = () => {
     const [categories, setCategories] = useState([])
+    const [showFixed, setShowFixed] = useState(false)
     useEffect(() => {
         const fetchCategories = async () => {
           const { data } = await axios.get(
@@ -17,12 +18,32 @@ const ListOfCategories = () => {
     
         fetchCategories();
       }, []);
-    return (
-        <List>
+
+      useEffect(() => {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed !== newShowFixed && setShowFixed(newShowFixed)
+        }
+      document.addEventListener('scroll', onScroll)
+        
+        return () => document.removeEventListener('scroll', onScroll)
+
+      }, [showFixed])
+      
+
+      const renderList = (fixed) => (
+        <List className={fixed ? 'fixed' : ''}>
             {
                 categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
             }
         </List>
+      )  
+    return (
+        <>
+        {renderList()}
+        {showFixed && renderList(true)}
+        </>
+        
     )
 }
 
