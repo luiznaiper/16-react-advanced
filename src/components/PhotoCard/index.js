9b5e3934eac5578713f23f65347cd386fa2a24a4
luiznaiper <luiz_naiper@hotmail.com>
2022-07-05 08:42:17 -0500

    :construction: Add intersection observer to load the images

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
-import {MdFavoriteBorder, mdFavoriteBorder} from 'react-icons/md'
+import React, {useEffect, useRef, useState} from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
+import {MdFavoriteBorder} from 'react-icons/md'
 
 const default_img = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 const PhotoCard = ({id, likes=0, src = default_img}) => {
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(function (entries){
+      const {isIntersecting} = entries[0]
+      console.log(isIntersecting)
+      if (isIntersecting){
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
+  
   return (
-    <article>
-        <a href={`/detail/${id}`} >
+    <Article ref={ref}>
+      {
+        show && <>
+           <a href={`/detail/${id}`} >
             <ImgWrapper>
                 <Img src={src}/>
             </ImgWrapper>
-        </a>
-        <Button>
-            <MdFavoriteBorder size='32px'/> {likes} likes!
-        </Button>
-    </article>
+           </a>
+          <Button>
+              <MdFavoriteBorder size='32px'/> {likes} likes!
+          </Button>
+        </>
+      }
+       
+    </Article>
   )
 }
 
