a6b0da04e63c275d6a4addfa09801b76acc93b5b
luiznaiper <luiz_naiper@hotmail.com>
2022-07-12 08:02:54 -0500

    :construction: Add user and notRegisteredUser with isAuth component fake to show diff routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import { Logo } from './components/Logo'
 import {NavBar} from './components/NavBar/'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const UserLogged = ({children}) =>{
+  return children({isAuth: false})
+}
 
 
 const App = () => {
@@ -17,9 +24,25 @@ const App = () => {
           <Logo/>
           <Routes>
              <Route path='/' element={<Home />} />
-             <Route path='/detail/:detailId' element={<Detail />} />
+             <Route path='/detail/:detailId' element={<Detail />} />             
              <Route path='/pet/:id' element={<Home />} />
            </Routes>
+           <UserLogged>
+            {
+              ({isAuth}) => 
+              isAuth
+              ?
+              <Routes>
+              <Route path='/favs' element={<Favs />} />
+              <Route path='/user' element={<User />} />
+              </Routes>
+              :
+              <Routes>
+                <Route path='/favs' element={<NotRegisteredUser />} />
+                 <Route path='/user' element={<NotRegisteredUser />} />
+              </Routes>
+            }            
+           </UserLogged>
            <NavBar/>
       </BrowserRouter>
     </>
