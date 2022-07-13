88581a1e007eed10f21c3f0504989c37a838960a
luiznaiper <luiz_naiper@hotmail.com>
2022-07-13 08:15:21 -0500

    :art: Add prettier to vs config

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,25 +1,24 @@
 import styled from "styled-components";
 import { Link as LinkRouter } from "react-router-dom";
-import { fadeIn } from '../../styles/animation';
-
+import { fadeIn } from "../../styles/animation";
 
 const Nav = styled.nav`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    position: fixed;
-    background: #fcfcfc;
-    border-top: 1px solid #e0e0e0;
-    height: 50px;
-    width: 100%;
-    max-width: 500px;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    margin: 0 auto;
-    z-index: 1000;
-    font-size: 32px;
-`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: fixed;
+  background: #fcfcfc;
+  border-top: 1px solid #e0e0e0;
+  height: 50px;
+  width: 100%;
+  max-width: 500px;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  margin: 0 auto;
+  z-index: 1000;
+  font-size: 32px;
+`;
 const Link = styled(LinkRouter)`
   display: inline-flex;
   align-items: center;
@@ -27,19 +26,19 @@ const Link = styled(LinkRouter)`
   color: #888;
   height: 100%;
   width: 100%;
-  text-decoration: none;    
-    &.selected {
-        color: #000;
-        background-color: lightgray;
-        &:after {
-        ${fadeIn({time:'0.5s'})}
-        content: '•';
-        position: absolute;
-        bottom: 0;
-        font-size: 34px;
-        line-height: 20px;
-        }
+  text-decoration: none;
+  &.selected {
+    color: #000;
+    background-color: lightgray;
+    &:after {
+      ${fadeIn({ time: "0.5s" })}
+      content: '•';
+      position: absolute;
+      bottom: 0;
+      font-size: 34px;
+      line-height: 20px;
     }
-`
+  }
+`;
 
-export {Nav, Link}
+export { Nav, Link };
